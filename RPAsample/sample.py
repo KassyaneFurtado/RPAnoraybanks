40a2ece9ce915df7246d89db830e9ec4d6fd23d5
KassyaneFurtado <kassyanefurtado@outlook.com>
2024-07-03 15:38:26 -0300

    Atualização do código

--- RPAsample/sample.py
+++ RPAsample/sample.py
@@ -3,11 +3,12 @@ from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import os
 import pandas as pd
-
+import pyautogui
 load_dotenv()
 login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
-sample = pd.read_csv("SAMPLE.csv")
+sitenoraybanks = os.getenv('NORAYBANKS')
+sample = pd.read_csv('SAMPLE.csv')
 positions = pd.read_csv('POSITIONS.csv')
 print(sample)
 print(positions)
@@ -15,14 +16,14 @@ print(positions)
 with sync_playwright() as p:
     navegador = p.chromium.launch(headless=False, args=['--ignore-certificate-errors'])
     noraybanks = navegador.new_page()
-    noraybanks.goto("https://noraybanks.mariopenna.org.br/InternalLoginNew.aspx")
+    noraybanks.goto(sitenoraybanks)
     noraybanks.wait_for_selector('//*[@id="nbCtrlLoginNew1_LoginUser_UserName"]')
     time.sleep(5)
     noraybanks.fill('//*[@id="nbCtrlLoginNew1_LoginUser_UserName"]',login) #LOGIN
     noraybanks.wait_for_selector('//*[@id="nbCtrlLoginNew1_LoginUser_Password"]')
     noraybanks.fill('//*[@id="nbCtrlLoginNew1_LoginUser_Password"]', password) #SENHA
     noraybanks.locator('//*[@id="nbCtrlLoginNew1_LoginUser_LoginButton_jqbtn"]').click()
-    
+    time.sleep(2)
     try:
         for linha in sample.index:
             noraybanks.wait_for_selector('//*[@id="wrapper-250"]/ul/li[1]/a')
@@ -46,8 +47,6 @@ with sync_playwright() as p:
                 time.sleep(3)
                 noraybanks.wait_for_event
                 noraybanks.query_selector('#nbConsulta1_NbCtrlBiobancoNodo1_ddListNodo').select_option(str(xpath))
-            else:
-                print(f"Posição não encontrada para o número {number}")
                 time.sleep(2)
 
             if not pd.isnull(sample.loc[linha, "PRONTUARIO"]):
@@ -57,16 +56,18 @@ with sync_playwright() as p:
                 noraybanks.fill('//*[@id="nbConsulta1_TxtCodigoUnico"]', prontuario)
 
             noraybanks.locator('//*[@id="btnFiltro_jqbtn"]').click()
+            time.sleep(2)
+            pyautogui.scroll(-200)
             noraybanks.wait_for_selector('//*[@id="GridList_ctl02_ImgbtnMod"]')
             noraybanks.locator('//*[@id="GridList_ctl02_ImgbtnMod"]').click()
+            time.sleep(2)
             noraybanks.wait_for_selector('//*[@id="GridDonacion_ctl02_ImgbtnMod"]')
             noraybanks.locator('//*[@id="GridDonacion_ctl02_ImgbtnMod"]').click()
+            time.sleep(5)
             noraybanks.locator('//*[@id="BtnMuestras_jqbtn"]').click()
             noraybanks.locator('//*[@id="DDListTipoM"]').click()
-            number = sample.loc[linha, 'TIPOA']
-            position = positions[positions['TIPO'] == number]
             if not positions.empty:
-                number = sample.loc[linha, 'TIPOC']
+                number = sample.loc[linha, 'TIPOA']
                 position = positions[positions['TIPO'] == number]
                 print(f"Processando número: {number}")
                 print(position)
@@ -74,62 +75,67 @@ with sync_playwright() as p:
                 xpath = position.iloc[0]['LOCADOR']
             # Selecionar a opção no menu suspenso
                 print(f"XPath selecionado: {xpath}")
-                time.sleep(10)
                 print(f"Selecionando a opção: {number} com XPath: {xpath}")
-                time.sleep(10)
-                noraybanks.locator('//*[@id="DDListTipoM"]').click()
-                noraybanks.locator('//*[@id="DDListTipoM"]').select_option(value=str(number))  # Supondo que a opção seja um valor string
+                time.sleep(3)
+                noraybanks.wait_for_event
+                noraybanks.query_selector('#DDListTipoM').select_option(str(xpath))
             else:
                 print(f"Posição não encontrada para o número {number}")
-                time.sleep(20)
+                time.sleep(5) 
             noraybanks.wait_for_selector('//*[@id="BtnNewMuestra_jqbtn"]')        
             noraybanks.locator('//*[@id="BtnNewMuestra_jqbtn"]').click() #ADICIONAR AMOSTRA
-            time.sleep(5)
+            time.sleep(10)
+            if not pd.isnull(sample.loc[linha, "DATA"]):
+                DATA = sample.loc[linha, "DATA"]
+                noraybanks.wait_for_selector('//*[@id="TxtFechaEntrada"]')
+                noraybanks.locator('//*[@id="TxtFechaEntrada"]').focus() #DATA DE ENTRADA
+                noraybanks.fill('//*[@id="TxtFechaEntrada"]', DATA) #DATA DE ENTRADA
+                time.sleep(2)
+                pyautogui.click(x=985, y=478)
+                time.sleep(5)
+                noraybanks.wait_for_selector('//*[@id="txtFechaObt"]')
+                noraybanks.locator('//*[@id="txtFechaObt"]')                             
+                noraybanks.locator('//*[@id="txtFechaObt"]').focus() #DATA DA AMOSTRA
+                noraybanks.locator('//*[@id="txtFechaObt"]').fill(DATA) #DATA DA AMOSTRA
+            time.sleep(2)
+            pyautogui.click(x=985, y=478)
+            time.sleep(2)
             noraybanks.wait_for_selector('//*[@id="TxtCodMuestra"]')
             codbarras = noraybanks.query_selector('//*[@id="TxtCodMuestra"]').input_value() #COPIAR COD DE AMOSTRA
+            time.sleep(2)
+            noraybanks.wait_for_selector('//*[@id="txtMicronic"]')
+            noraybanks.locator('//*[@id="txtMicronic"]').click()
+            noraybanks.query_selector('//*[@id="txtMicronic"]').fill(codbarras) #ESCREVER COD DE BARRAS
             time.sleep(1)
-            noraybanks.wait_for_selector('//*[@id="TxtCodEntrada"]')
-            noraybanks.locator('//*[@id="TxtCodEntrada"]').click()
-            noraybanks.query_selector('//*[@id="TxtCodEntrada"]').fill(codbarras) #ESCREVER COD DE BARRAS
-            time.sleep(1)
-            noraybanks.wait_for_selector('//*[@id="TxtFechaEntrada"]')
-            noraybanks.locator('//*[@id="TxtFechaEntrada"]').click()
-            DATA = str(int(sample.loc[linha, "DATA"])) 
-            noraybanks.fill('//*[@id="TxtFechaEntrada"]', DATA) #DATA DE ENTRADA
-            time.sleep(1)
-            noraybanks.wait_for_selector('//*[@id="TxtVolumen"]')
-            noraybanks.locator('//*[@id="TxtVolumen"]').click()
-            VOLUME = str(int(sample.loc[linha, "VOLUME"]))
-            noraybanks.fill('//*[@id="TxtVolumen"]', VOLUME) #VOLUME
-            time.sleep(1)
-            noraybanks.wait_for_selector('//*[@id="txtFechaObt"]')
-            noraybanks.locator('//*[@id="txtFechaObt"]')                             
-            noraybanks.fill('//*[@id="txtFechaObt"]', DATA) #DATA DA AMOSTRA
-            time.sleep(10)
             noraybanks.wait_for_selector('//*[@id="DDListEstadoMuetra"]')
-            noraybanks.locator('//*[@id="DDListEstadoMuetra"]').select_option('2') #STATUS AMOSTRA
-            time.sleep(1)
-            CASO = str(int(sample.loc[linha, "CASO"]))
+            noraybanks.locator('//*[@id="DDListEstadoMuetra"]').select_option('1') #STATUS AMOSTRA
+            CASO = str(sample.loc[linha, "CASO"])
             noraybanks.wait_for_selector('//*[@id="TxtCodEntrada"]')
             noraybanks.locator('//*[@id="TxtCodEntrada"]').click()
             noraybanks.fill('//*[@id="TxtCodEntrada"]', CASO) #COD DE ENTRADA
+            noraybanks.wait_for_selector('//*[@id="TxtVolumen"]')
+            noraybanks.locator('//*[@id="TxtVolumen"]').click()
+            VOLUME = str(float(sample.loc[linha, "VOLUME"])).replace('.', ',')
+            noraybanks.fill('//*[@id="TxtVolumen"]', VOLUME) #VOLUME
             time.sleep(1)
-            noraybanks.locator('//*[@id="DDListResponsable"]').click()
-            noraybanks.locator('//*[@id="DDListResponsable"]/option[3]').click()
+            noraybanks.locator('#DDListResponsable').select_option('2')
             time.sleep(1)
+            pyautogui.scroll(-500)
+            time.sleep(5)
             if not pd.isnull(sample.loc[linha, "OBS"]):
                 noraybanks.locator('//*[@id="TxtObs1"]').click()
                 OBS = str(int(sample.loc[linha, "OBS"]))
                 noraybanks.fill('//*[@id="TxtObs1"]', OBS)
-            noraybanks.locator('//*[@id="btnSave_jqbtn"]/span[1]') #SALVAR
-            noraybanks.locator('//*[@id="btnImprimirEtiquetasTL_jqbtn"]').click() #IMPRESSÃO DIRETA
-            noraybanks.locator('//*[@id="nbctrlImprimirEtiquetas1_ddListLabelDesign"] ').click() #ESCOLHER DESING DA ETIQUETA
-            #noraybanks.locator('')
+            noraybanks.locator('//*[@id="btnSave_jqbtn"]/span[1]').click() #SALVAR
             time.sleep(5)
-            #pyautogui.click(x=412, y=653)
+            noraybanks.wait_for_selector('//*[@id="NbctrlPopup1_ButtonOk_jqbtn"]')
+            noraybanks.locator('//*[@id="NbctrlPopup1_ButtonOk_jqbtn"]').click()
+            time.sleep(7)
+            pyautogui.scroll(2000)
+            noraybanks.wait_for_selector('//*[@id="wrapper-250"]/ul/li[1]/a')
 
     finally:
-        noraybanks.locator('//*[@id="Header1_NbCurrentProfile1_ButtonLogOut_jqbtn"]').click()
-        time.sleep(5)
+        noraybanks.query_selector('#Header1_NbCurrentProfile1_ButtonLogOut_jqbtn').click()
+        time.sleep(20)
         navegador.close()
 
